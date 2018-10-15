1710667544412b1d8eab300e601297a84090cf32
Vladimir Vinogradov <vinogradovve@gmail.com>
2018-10-15 22:21:29 +0500

    Add SendWithHeader method for HTTP client

--- httpclient/client.go
+++ httpclient/client.go
@@ -22,6 +22,11 @@ type VirgilHttpClient struct {
 }
 
 func (c *VirgilHttpClient) Send(ctx context.Context, method string, url string, payload interface{}, respObj interface{}) error {
+	_, err := c.SendWithHeader(ctx, method, url, payload, nil, respObj)
+	return err
+}
+
+func (c *VirgilHttpClient) SendWithHeader(ctx context.Context, method string, url string, payload interface{}, header http.Header, respObj interface{}) (http.Header, error) {
 	var reqBody []byte
 	var err error
 
@@ -34,35 +39,38 @@ func (c *VirgilHttpClient) Send(ctx context.Context, method string, url string, 
 		default:
 			reqBody, err = json.Marshal(payload)
 			if err != nil {
-				return errors.Wrap(err, "HttpClient.Send [JSON marshal payload]")
+				return nil, errors.Wrap(err, "HttpClient.Send [JSON marshal payload]")
 			}
 		}
 	}
 	req, err := http.NewRequest(method, fmt.Sprint(c.ServiceAddress, url), bytes.NewReader(reqBody))
 	if err != nil {
-		return errors.Wrapf(err, "HttpClient.Send [Method: %s Path: %s ]", method, url)
+		return nil, errors.Wrapf(err, "HttpClient.Send [Method: %s Path: %s ]", method, url)
+	}
+	for k := range header {
+		req.Header.Add(k, header.Get(k))
 	}
 	req.Header.Add("content-type", "application/json")
 
 	resp, err := c.getHTTPClient().Do(req)
 	if err != nil {
-		return errors.Wrapf(err, "HttpClient.Send [Send request]")
+		return nil, errors.Wrapf(err, "HttpClient.Send [Send request]")
 	}
 	defer resp.Body.Close()
 	switch resp.StatusCode {
 	case http.StatusNotFound:
-		return apierror.EntityNotFoundErr
+		return resp.Header, apierror.EntityNotFoundErr
 	case http.StatusInternalServerError:
-		return apierror.InternalServerErr
+		return resp.Header, apierror.InternalServerErr
 	case http.StatusUnauthorized:
-		return apierror.UnauthorizedRequestErr
+		return resp.Header, apierror.UnauthorizedRequestErr
 	case http.StatusForbidden:
-		return apierror.ForbiddenErr
+		return resp.Header, apierror.ForbiddenErr
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return errors.Wrapf(err, "HttpClient.Send [ReadBody (Method: %s Path: %s Body: %s)]", method, url, reqBody)
+		return resp.Header, errors.Wrapf(err, "HttpClient.Send [ReadBody (Method: %s Path: %s Body: %s)]", method, url, reqBody)
 	}
 
 	if resp.StatusCode < 200 || resp.StatusCode > 299 { // http status code seccess
@@ -70,22 +78,22 @@ func (c *VirgilHttpClient) Send(ctx context.Context, method string, url string, 
 		if len(body) != 0 {
 			err = json.Unmarshal(body, &verr)
 			if err != nil {
-				return errors.Wrapf(err, "HttpClient.Send [UnmarshalResponseErr(status code: %v body: %s)]", resp.StatusCode, body)
+				return resp.Header, errors.Wrapf(err, "HttpClient.Send [UnmarshalResponseErr(status code: %v body: %s)]", resp.StatusCode, body)
 			}
 		}
 		verr.StatusCode = resp.StatusCode
-		return verr
+		return resp.Header, verr
 	}
 
 	if respObj == nil {
-		return nil
+		return resp.Header, nil
 	}
 
 	err = json.Unmarshal(body, respObj)
 	if err != nil {
-		return errors.Wrapf(err, "HttpClient.Send [UnmarshalResponseErr(status code: %v body: %s)]", resp.StatusCode, body)
+		return resp.Header, errors.Wrapf(err, "HttpClient.Send [UnmarshalResponseErr(status code: %v body: %s)]", resp.StatusCode, body)
 	}
-	return nil
+	return resp.Header, nil
 }
 
 func (c *VirgilHttpClient) getHTTPClient() HTTPClient {
